ae4e365bb3bcb991f90e6997e22b1f33bad18b67
Thomas marchiotti <Thomas@MBP-di-Thomas.lan>
2022-04-18 18:05:45 +0200

    Aggiunto colore

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var yoff = 0;
+var c;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	var bg_color_r = random(0, 250);
-	var bg_color_g = random(0, 250);
-	var bg_color_b = random(0, 250);
+	colorMode(HSB, 360, 100, 100);
+	c = 0;
 }
 
 function draw() {
@@ -12,7 +12,9 @@ function draw() {
 	translate(width / 2, height / 2);
 
 	stroke(255);
-	fill(255, 50);
+	fill(c, 80, 100);
+	c += 1;
+	c = c%360;
 	strokeWeight(1);
 
 	var da = PI / 200;
@@ -21,6 +23,7 @@ function draw() {
 	var xoff = 0;
 	beginShape();
 	for (var a = 0; a <= TWO_PI; a += da) {
+
 		var n = noise(xoff, yoff);
 		var r = sin(2 * a) * map(n, 0, 1, 50, 300);
 		var x = r * cos(a);
@@ -30,9 +33,13 @@ function draw() {
 		} else {
 			xoff -= dx;
 		}
+
 		vertex(x, y);
+		
 	}
+	
 	endShape();
 
 	yoff += 0.01;
 }
+
