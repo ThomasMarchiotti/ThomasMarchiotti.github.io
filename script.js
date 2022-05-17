7840774f5a89b37f97aedec875a19e3166fb1b87
Thomas marchiotti <Thomas@MBP-di-Thomas.lan>
2022-05-17 19:52:27 +0200

    modificati colori

--- script.js
+++ script.js
@@ -18,6 +18,23 @@ function draw() {
 	// fill(c, 500, 70);
 	// c += 1;
 	// c = c%360;
+
+	const m = 100;
+  
+	const topR = 255 * noise(frameCount / m);
+	const topG = 255 * noise(1000 + frameCount / m);
+	const topB = 255 * noise(2000 + frameCount / m);
+	const bottomR = 255 * noise(3000 + frameCount / m);
+	const bottomG = 255 * noise(4000  + frameCount / m);
+	const bottomB = 255 * noise(5000 + frameCount / m);
+  
+	const topColor = color(topR, topG, topB);
+	const bottomColor = color(bottomR, bottomG, bottomB);
+
+	for(let y = 0; y < height; y++) {
+		fill(lerpColor(topColor, bottomColor, y / height), 500, 70);
+	}
+
 	strokeWeight(1);
 
 	var da = PI / 200;
